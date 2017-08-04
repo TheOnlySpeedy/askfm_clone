369d01e7b9e2ae9736f08fd64259fab84f3dd98f
Florian Schmid <speedyschmid@gmx.net>
2017-08-04 19:09:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthHttp } from 'angular2-jwt';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Ask.fm-Clone';
+  thing;
+
+  constructor(private authHttp: AuthHttp, private http: Http) {}
+
+  ngOnInit() {
+    const myHeader = new Headers();
+    myHeader.append('Content-Type', 'application/json');
+    myHeader.append('Php-Auth-User', 'asdffdsa');
+    myHeader.append('Php-Auth-Pw', 'asdffdsa');
+    const options = new RequestOptions({ headers: myHeader });
+
+    this.http.post('http://api.askfm.apdev.idowapro.de/tokens', null , options)
+      .subscribe(
+        data => this.thing = data,
+        err => console.log(err),
+        () => console.log('Request complete', this.thing)
+      );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule, Http, RequestOptions } from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth.module';
+import { AuthConfig, AuthHttp } from 'angular2-jwt';
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp(new AuthConfig({
+    globalHeaders: [{'Content-Type': 'application/json'}],
+    noTokenScheme: true
+  }), http, options);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [Http, RequestOptions]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
